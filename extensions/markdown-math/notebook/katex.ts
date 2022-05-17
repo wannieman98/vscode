aa6834f0ec211128bae3c03d47a1032ce2a7f3da
wannieman98 <74849806+wannieman98@users.noreply.github.com>
2022-05-18 06:08:53 +0900

    using wannieman98 for debugging purposes

--- extensions/markdown-math/notebook/katex.ts
+++ extensions/markdown-math/notebook/katex.ts
@@ -42,7 +42,7 @@ export async function activate(ctx: RendererContext<void>) {
 	styleTemplate.content.appendChild(link);
 	document.head.appendChild(styleTemplate);
 
-	const katex = require('@iktakahiro/markdown-it-katex');
+	const katex = require('@wannieman98/markdown-it-katex');
 	const macros = {};
 	markdownItRenderer.extendMarkdownIt((md: markdownIt.MarkdownIt) => {
 		return md.use(katex, {
